89645a18dadeec6da7b13a116b5ed18968231937
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-19 19:47:53 +0600

    Delete functionality added

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
