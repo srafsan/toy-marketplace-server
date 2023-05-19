dfe487fc31b2874e22ca31c2acc42d4de1ae94f4
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-19 11:25:46 +0600

    Toys Get API added

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db("ToysDB").collection("toys");
+
+    app.get("/toys", async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
